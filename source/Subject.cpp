54eb50860188bbd785b4a422a61834ba44fe01a3
inte <inte.vleminckx14@hotmail.com>
2021-11-16 17:27:55 +0100

    Adding classes

---
+++ source/Subject.cpp
@@ -0,0 +1,5 @@
+//
+// Created by inte on 11/16/21.
+//
+
+#include "Subject.h"
---
+++ source/Subject.h
@@ -0,0 +1,21 @@
+//
+// Created by inte on 11/16/21.
+//
+
+#ifndef DOODLE_JUMP_SUBJECT_H
+#define DOODLE_JUMP_SUBJECT_H
+
+#include <iostream>
+
+
+using namespace std;
+
+class Subject {
+public:
+
+    Subject();
+
+};
+
+
+#endif //DOODLE_JUMP_SUBJECT_H
